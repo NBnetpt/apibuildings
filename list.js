46a32b37c1f9c7f84c3670c4965e484b083ba79b
NBnetpt <74501999+NBnetpt@users.noreply.github.com>
2026-01-29 06:06:51 +0000

    Add JSON data for various stations

---
+++ list.js
@@ -0,0 +1,212 @@
+{
+  "stations": [
+    {
+      "name": "233 Broadway",
+      "notes": "The Woolworth Building",
+      "image": "",
+      "sheet": "",
+      "type": "private",
+      "uuid": "a2e27ccb-0982-4d45-889b-c643b1617028",
+      "areas": [
+        [
+          [
+            -13993,
+            -7834
+          ],
+          [
+            -13951,
+            -7804
+          ]
+        ],
+        [
+          [
+            -13993,
+            -7834
+          ],
+          [
+            -13951,
+            -7804
+          ]
+        ]
+      ],
+      "x": -13960,
+      "z": -7819
+    },
+    {
+      "name": "225 W 86th St",
+      "notes": "The Belnord Apartments",
+      "type": "nycha",
+      "image": "https://i.ibb.co/hxZN3Tpg/A9kR1Bz.png",
+      "uuid": "ec3c3b63-4158-4a7e-bb6c-02a82271d709",
+      "areas": [
+        [
+          [
+            -14027,
+            -7887
+          ],
+          [
+            -13951,
+            -7841
+          ]
+        ]
+      ],
+      "x": -13989,
+      "z": -7865,
+      "sheet": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS2ZuEFwp00sW4ePb7Eqa3_LBdfO7eot1QZkR0qI5DlYVW1ZD6zDyuEtq4PKYPvCWMXXAYpzP9yyq22/pubhtml?gid=984770259&amp;single=true&amp;widget=true&amp;headers=false"
+    },
+    {
+      "name": "232 W 85th St",
+      "notes": "232 W 85th St",
+      "type": "nycha",
+      "uuid": "ec3g3b63-4158-4a7e-bb6c-02a82271d709",
+      "areas": [
+        [
+          [
+            -14002,
+            -7834
+          ],
+          [
+            -13993,
+            -7804
+          ]
+        ]
+      ],
+      "x": -14000,
+      "z": -7833,
+      "sheet": "",
+      "image": "https://cdn-0001.qstv.on.epicgames.com/ymBhRCqxhRdnMNbJqE/image/landscape_comp.jpeg"
+    },
+    {
+      "name": "20 Exchange Place",
+      "notes": "City Bankâ€“Farmers Trust Building",
+      "type": "private",
+      "uuid": "ec3g3b63-4158-4a7e-b26c-02a82271d709",
+      "areas": [
+        [
+          [
+            -13839,
+            -7793
+          ],
+          [
+            -13818,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13818,
+            -7780
+          ],
+          [
+            -13817,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13817,
+            -7778
+          ],
+          [
+            -13816,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13816,
+            -7776
+          ],
+          [
+            -13815,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13815,
+            -7774
+          ],
+          [
+            -13814,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13814,
+            -7772
+          ],
+          [
+            -13813,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13813,
+            -7770
+          ],
+          [
+            -13812,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13812,
+            -7768
+          ],
+          [
+            -13811,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13811,
+            -7766
+          ],
+          [
+            -13810,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13810,
+            -7764
+          ],
+          [
+            -13809,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13809,
+            -7762
+          ],
+          [
+            -13808,
+            -7753
+          ]
+        ],
+        [
+          [
+            -13808,
+            -7761
+          ],
+          [
+            -13807,
+            -7755
+          ]
+        ]
+      ],
+      "x": -13828,
+      "z": -7792,
+      "sheet": "",
+      "image": "https://images.squarespace-cdn.com/content/v1/5bb7babee666697963b7619e/1545349977531-NGIS5T56JJ2GU9OJJ3LZ/20Exchange.JPG"
+    }
+  ]
+}
